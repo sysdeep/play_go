03ab34a646dc589403ac2b27f4e96fdb9a93191f
Igor Nefedkin <igor.nefedkin@kaspersky.com>
2024-05-18 14:56:42 +0300

    networks, secrets and configs lists

--- projects/go_hard_docker/hdu/internal/webserver/handlers/page_secrets.go
+++ projects/go_hard_docker/hdu/internal/webserver/handlers/page_secrets.go
@@ -1,14 +1,23 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"sort"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/swarm"
 	"github.com/labstack/echo/v4"
 )
 
 // models
 type secretListModel struct {
+	ID      string
+	Name    string
+	Created string
+	Updated string
 }
+
 type secretsPageModel struct {
 	Secrets []secretListModel
 	Total   int
@@ -16,24 +25,34 @@ type secretsPageModel struct {
 
 // handler
 func (h *Handlers) SecretsPage(c echo.Context) error {
-	// volumes_data, err := h.docker_client.VolumeList(context.Background(), volume.ListOptions{})
-	// if err != nil {
-	// 	return echo.NewHTTPError(http.StatusInternalServerError, err)
-	// }
-	//
-	// var volumes []volumeListModel
-	// for _, v := range volumes_data.Volumes {
-	// 	volumes = append(volumes, make_volume_list_model(v))
-	// }
-	//
-	// sort.SliceStable(volumes, func(i, j int) bool {
-	// 	return volumes[i].Name < volumes[j].Name
-	// })
+	secrets_data, err := h.docker_client.SecretList(context.Background(), types.SecretListOptions{})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	var secrets []secretListModel
+	for _, v := range secrets_data {
+		secrets = append(secrets, make_secret_list_model(v))
+	}
+
+	sort.SliceStable(secrets, func(i, j int) bool {
+		return secrets[i].Name < secrets[j].Name
+	})
 
 	response := secretsPageModel{
-		Secrets: *new([]secretListModel),
-		Total:   0,
+		Secrets: secrets,
+		Total:   len(secrets),
 	}
 
 	return c.Render(http.StatusOK, "secrets.html", response)
+}
+
+func make_secret_list_model(model swarm.Secret) secretListModel {
+	// utils.PrintAsJson(model)
+	return secretListModel{
+		ID:      model.ID,
+		Name:    model.Spec.Name,
+		Created: model.CreatedAt.String(),
+		Updated: model.UpdatedAt.String(),
+	}
 }
