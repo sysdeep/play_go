66868544ad938e3602dd25845106da0292108bb5
Igor Nefedkin <igor.nefedkin@kaspersky.com>
2024-07-29 19:20:15 +0300

    add volumes template

---
+++ projects/tkdocker/tkdocker/internal/ui/volumes_page_ctrl.go
@@ -0,0 +1,24 @@
+package ui
+
+// идея чтобы вся логика была тут
+type VolumesPageCtrl struct{}
+
+func NewVolumesPageCtrl() *VolumesPageCtrl {
+	return &VolumesPageCtrl{}
+}
+
+func (v *VolumesPageCtrl) GetVolumes() {
+
+}
+
+func (v *VolumesPageCtrl) SelectVolume() {
+
+}
+
+func (v *VolumesPageCtrl) DeleteVolume() {
+
+}
+
+func (v *VolumesPageCtrl) PruneVolumes() {
+
+}
