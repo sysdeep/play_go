bbe4e93bea7337990ccd8aea70e3bcb09c9bc511
Igor Nefedkin <igor.nefedkin@kaspersky.com>
2024-07-30 19:23:07 +0300

    try sidebar

--- projects/tkdocker/tkdocker/internal/ui/main_window.go
+++ projects/tkdocker/tkdocker/internal/ui/main_window.go
@@ -17,7 +17,46 @@ func NewMainWindow(servs *services.Services) *MainWindow {
 		tk.RootWindow(),
 	}
 
-	tabs := tk.NewNotebook(mw)
+	// content
+	content_frame := tk.NewFrame(mw)
+	content_layout := tk.NewHPackLayout(content_frame)
+
+	sidebar := NewNavSidebar(mw)
+	tabs := mw.makeTabs(mw, servs)
+
+	content_layout.AddWidget(sidebar, tk.PackAttrFillY(), tk.PackAttrSideLeft())
+	content_layout.AddWidget(tabs, tk.PackAttrFillY(), tk.PackAttrSideRight())
+
+	actions_bar := mw.makeActionsBar(mw)
+
+	main_layout := tk.NewVPackLayout(mw)
+	main_layout.AddWidget(content_frame,
+		tk.PackAttrFillBoth(),
+		tk.PackAttrExpand(true),
+		tk.PackAttrPadx(4),
+		tk.PackAttrPady(4),
+	)
+	// layout.AddWidget(tabs,
+	// 	tk.PackAttrFillBoth(),
+	// 	tk.PackAttrExpand(true),
+	// 	tk.PackAttrPadx(4),
+	// 	tk.PackAttrPady(10))
+	//
+	// layout.AddWidget(tk.NewLayoutSpacer(mw, 0, true))
+	main_layout.AddWidget(actions_bar,
+		tk.PackAttrSideBottom(),
+		tk.PackAttrExpand(false),
+		tk.PackAttrFillX(),
+		tk.PackAttrPadx(4),
+		tk.PackAttrPady(4),
+	)
+
+	return mw
+
+}
+
+func (mw *MainWindow) makeTabs(root tk.Widget, servs *services.Services) *tk.Notebook {
+	tabs := tk.NewNotebook(root)
 
 	containers_page := NewContainersPage(tabs, servs.Containers)
 	tabs.AddTab(containers_page, "Containers")
@@ -31,21 +70,23 @@ func NewMainWindow(servs *services.Services) *MainWindow {
 	test_label := tk.NewLabel(tabs, "test")
 	tabs.AddTab(test_label, "Test")
 
-	btn := tk.NewButton(mw, "Quit")
-	btn.OnCommand(func() {
-		tk.Quit()
-	})
+	return tabs
+}
 
-	layout := tk.NewVPackLayout(mw)
-	layout.AddWidget(tabs,
-		tk.PackAttrFillBoth(),
-		tk.PackAttrExpand(true),
-		tk.PackAttrPadx(4),
-		tk.PackAttrPady(10))
+func (mw *MainWindow) makeActionsBar(root tk.Widget) *tk.Frame {
+	fr := tk.NewFrame(root)
 
-	// layout.AddWidget(tk.NewLayoutSpacer(mw, 0, true))
-	layout.AddWidget(btn, tk.PackAttrSideBottom())
+	exit_btn := tk.NewButton(mw, "Exit")
+	exit_btn.OnCommand(mw.exit)
 
-	return mw
+	layout := tk.NewHPackLayout(fr)
+	layout.AddWidget(tk.NewLayoutSpacer(fr, 1, true))
+	layout.AddWidget(exit_btn)
 
+	return fr
+}
+
+func (mw *MainWindow) exit() {
+
+	tk.Quit()
 }
